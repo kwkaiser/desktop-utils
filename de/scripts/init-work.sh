6e952e0016c1e137d4481e13093a437e750ae809
Karl Kaiser <karl@kwkaiser.io>
2022-06-08 07:18:42 -0400

    Finch work setup

--- de/scripts/init-work.sh
+++ de/scripts/init-work.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-swaymsg 'workspace 7'
+swaymsg 'workspace 8'
 swaymsg 'splith'
 swaymsg 'layout tabbed'
 
-code ~/documents/miltenyi/code/data-platform-deployment
-code ~/documents/miltenyi/code/data-platform-database
-code ~/documents/miltenyi/code/data-platform-identity
-code ~/documents/miltenyi/code/data-platform-frontend
-code ~/documents/miltenyi/code/data-platform-backend
-
+chromium --incognito 'gmail.com' 'app.slack.com' 'calendar.google.com' &
+sleep 2s 
+chromium --incognito --new-window 'asana.com' 'notion.com'
